b7fb2643eb3e139278e1d837ec08bb4f04da8f57
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-01-22 17:57:26 -0600

    updates.

--- compute_stats.pyx
+++ compute_stats.pyx
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 import datetime
 from datetime import date
 import json
@@ -6,7 +7,6 @@ import time
 import numpy
 import requests
 
-from libc.stdlib cimport atof
 
 def get_crumb(response):
     """Parses out the crumb needed for the CSV download request."""
@@ -35,47 +35,41 @@ def get_title(response):
 
 def get_adj_close_and_changes(response_text):
     """Extracts prices from text and computes daily changes."""
-    start = time.time_ns()
-
-    cdef int i
-    cdef double adj_close
+    #start = time.time_ns()
 
     lines = response_text.split('\n')
     data_lines = lines[1:-1]
-    cdef int len_data_lines = len(data_lines)
-
+    len_data_lines = len(data_lines)
     adj_prices = numpy.zeros(len_data_lines, dtype=float)
-    cdef double[:] adj_prices_view = adj_prices
-
     changes = numpy.zeros(len_data_lines - 1, dtype=float)
-    cdef double[:] changes_view = changes
-
-    for i in range(len_data_lines):
-        cols = data_lines[i].split(',')
+    for i, line in enumerate(data_lines):
+        cols = line.split(',')
         if cols[5] == 'null':
             print('===== "null" values found in the input ====')
             print('===== continuing ..... ====================')
             return (None, None)
         adj_close = float(cols[5])
-        adj_prices_view[i] = adj_close
+        adj_prices[i] = adj_close
         if i:
-            changes_view[i-1] = (adj_close - adj_prices_view[i-1])/adj_prices_view[i-1]
-    end = time.time_ns()
-    print('ran get_adj_close_and_changes() in %d.' % (end - start))
+            changes[i-1] = (adj_close - adj_prices[i-1])/adj_prices[i-1]
+    #end = time.time_ns()
+    #print('ran get_adj_close_and_changes() in %d.' % (end - start))
 
     return (adj_prices, changes)
 
 def compute_sign_diff_pct(ticker_changes):
+    """Computes sign-diffs for up and down 10 and 20 blocks."""
     changes_0 = ticker_changes[1:-1]
     changes_minus_one = ticker_changes[:-2]
 
-    changes_tuples = zip(changes_minus_one, changes_0)
-    sorted_descending = list(reversed(sorted(changes_tuples, key=lambda b: b[0])))
+    # You are trying to fix this up.
+    changes_tuples = numpy.column_stack([changes_minus_one, changes_0])
+    sorted_descending = changes_tuples[changes_tuples[:, 0].argsort()[::-1]]
 
     # UP
     pct_sum_10_up = 0
     pct_sum_20_up = 0
-    np_avg_10_up = numpy.zeros(10)
+    np_avg_10_up = numpy.zeros(10, dtype=float)
     for i, ele in enumerate(sorted_descending[:20]):
         product = ele[0] * ele[1]
         if i < 10:
@@ -91,7 +85,7 @@ def compute_sign_diff_pct(ticker_changes):
     # DOWN
     pct_sum_10_down = 0
     pct_sum_20_down = 0
-    np_avg_10_down = numpy.zeros(10)
+    np_avg_10_down = numpy.zeros(10, dtype=float)
     for i, ele in enumerate(sorted_descending[-20:]):
         product = ele[0] * ele[1]
         if i > 9:
@@ -156,14 +150,20 @@ def process_ticker(ticker, manana_stamp, ago_366_days_stamp):
                     'period1=%d&period2=%d&interval=1d&events=history'
                     '&crumb=%s' % (ticker, ago_366_days_stamp, manana_stamp, crumb))
     print('download_url = %s' % download_url)
-    download_response = requests.get(download_url, cookies=cookie_jar)
+
+    title = None
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        request_future = executor.submit(requests.get, download_url, cookies=cookie_jar)
+        title_future = executor.submit(get_title, response)
+        title = title_future.result()
+        download_response = request_future.result()
 
     adj_close, changes_daily = get_adj_close_and_changes(download_response.text)
     if adj_close is None:
         return None
 
     sigma_data = get_sigma_data(changes_daily)
-    sigma_data['c_name'] = get_title(response)
+    sigma_data['c_name'] = title
     sigma_data['c_ticker'] = ticker
     return sigma_data
 
