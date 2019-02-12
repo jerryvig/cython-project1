f7b79b3386cd40459b7249d16a174caa11f2372b
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-02-12 17:06:03 -0600

    updates

--- compute_statistics.c
+++ compute_statistics.c
@@ -432,9 +432,7 @@ int main(void) {
     memset(sign_diff_print, 0, 512);
     char temp_str[128];
     memset(temp_str, 0, 128);
-
     strcat(sign_diff_print, "===============================\n");
-
     sprintf(temp_str, "  \"avg_move_10_down\": %s\n  \"avg_move_10_up\": %s\n", sign_diff_values.avg_move_10_down, sign_diff_values.avg_move_10_up);
     strcat(sign_diff_print, temp_str);
     memset(temp_str, 0, 128);
@@ -446,17 +444,17 @@ int main(void) {
     memset(temp_str, 0, 128);
     sprintf(temp_str, "  \"sigma\": %s\n  \"sigma_change\": %s\n", sign_diff_values.sigma, sign_diff_values.sigma_change);
     strcat(sign_diff_print, temp_str);
+    memset(temp_str, 0, 128);
+    sprintf(temp_str, "  \"sign_diff_pct_10_down\": %s\n  \"sign_diff_pct_10_up\": %s\n", sign_diff_values.sign_diff_pct_10_down, sign_diff_values.sign_diff_pct_10_up);
+    strcat(sign_diff_print, temp_str);
+    memset(temp_str, 0, 128);
+    sprintf(temp_str, "  \"sign_diff_pct_20_down\": %s\n  \"sign_diff_pct_20_up\": %s\n", sign_diff_values.sign_diff_pct_20_down, sign_diff_values.sign_diff_pct_20_up);
+    strcat(sign_diff_print, temp_str);
+    memset(temp_str, 0, 128);
+    sprintf(temp_str, "  \"stdev_10_down\": %s\n  \"stdev_10_up\": %s\n", sign_diff_values.stdev_10_down, sign_diff_values.stdev_10_up);
+    strcat(sign_diff_print, temp_str);
 
     printf("sign_diff_print = %s", sign_diff_print);
-
-    /* printf("  \"avg_move_10_down\": %s\n  \"avg_move_10_up\": %s\n", sign_diff_values.avg_move_10_down, sign_diff_values.avg_move_10_up);
-    printf("  \"title\": \"%s\"\n  \"change\": %s\n", sign_diff_values.title, sign_diff_values.change);
-    printf("  \"record_count\": %s\n  \"self_correlation\": %s\n", sign_diff_values.record_count, sign_diff_values.self_correlation);
-    printf("  \"sigma\": %s\n  \"sigma_change\": %s\n", sign_diff_values.sigma, sign_diff_values.sigma_change);
-    printf("  \"sign_diff_pct_10_down\": %s\n  \"sign_diff_pct_10_up\": %s\n", sign_diff_values.sign_diff_pct_10_down, sign_diff_values.sign_diff_pct_10_up);
-    printf("  \"sign_diff_pct_20_down\": %s\n  \"sign_diff_pct_20_up\": %s\n", sign_diff_values.sign_diff_pct_20_down, sign_diff_values.sign_diff_pct_20_up);
-    printf("  \"stdev_10_down\": %s\n  \"stdev_10_up\": %s\n", sign_diff_values.stdev_10_down, sign_diff_values.stdev_10_up);
-*/
     exit(0);
 
     const CURL *curl = curl_easy_init();
