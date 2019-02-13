39943b6b2aba7441975f7fb335224f9a9d1638a1
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-02-12 18:53:51 -0600

    abc

--- stats_fastcgi.c
+++ stats_fastcgi.c
@@ -7,6 +7,10 @@ int main (void) {
     while (FCGI_Accept() >= 0) {
         char *query_string = getenv("QUERY_STRING");
         char *path_info = getenv("PATH_INFO");
+        sign_diff_pct sign_diff_values;
+        run_stats("HON", &sign_diff_values);
+        char sign_diff_print[512];
+        build_sign_diff_print_string(sign_diff_print, &sign_diff_values);
 
         printf("Status: 200 OK\r\n");
         printf("Content-type: text/html\r\n\r\n");
@@ -18,11 +22,6 @@ int main (void) {
         if (query_string != NULL) {
             printf("query_string = &quot;%s&quot;\n", query_string);
         }
-
-        sign_diff_pct sign_diff_values;
-        run_stats("FB", &sign_diff_values);
-        char sign_diff_print[512];
-        build_sign_diff_print_string(sign_diff_print, &sign_diff_values);
         printf("%s", sign_diff_print);
 
         printf("</pre></body></html>\n");
