ee9d838a2f46ad2caf2cfdcacba2cb2eafde78ba
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-02-12 19:41:31 -0600

    Updates

--- stats_fastcgi.c
+++ stats_fastcgi.c
@@ -7,24 +7,29 @@ int main (void) {
     while (FCGI_Accept() >= 0) {
         char *query_string = getenv("QUERY_STRING");
         char *path_info = getenv("PATH_INFO");
+
         sign_diff_pct sign_diff_values;
-        run_stats("HON", &sign_diff_values);
+        if (path_info != NULL) {
+            if (strlen(path_info) > 1) {
+                run_stats(&path_info[1], &sign_diff_values);
+            }
+        }
         char sign_diff_print[512];
         build_sign_diff_print_string(sign_diff_print, &sign_diff_values);
 
         printf("Status: 200 OK\r\n");
         printf("Content-type: text/html\r\n\r\n");
-        printf("<!doctype><html><body><pre>Hola mundo from fastcgi with lighttpd!\n");
+        printf("<!doctype><html><head></head><body style=\"width: 100%%;\"><div style=\"width: 40%%;margin: 0 auto;\"><pre>Hola mundo from fastcgi with lighttpd!\n");
 
-        if (path_info != NULL) {
-            printf("path_info = &quot;%s&quot;\n", path_info);
-        }
-        if (query_string != NULL) {
+        if (query_string != NULL && strlen(query_string) > 0) {
             printf("query_string = &quot;%s&quot;\n", query_string);
         }
-        printf("%s", sign_diff_print);
 
-        printf("</pre></body></html>\n");
+        if (path_info != NULL) {
+            printf("%s", sign_diff_print);
+        }
+
+        printf("</pre></div></body></html>\n");
     }
 
     return EXIT_SUCCESS;
