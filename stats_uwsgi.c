5fb62d3080e6d2b5cdd83e8de358572304bac24b
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-02-21 18:10:22 -0600

    updates

--- stats_uwsgi.c
+++ stats_uwsgi.c
@@ -13,10 +13,18 @@ int stats_uwsgi(struct wsgi_request *req) {
     memset(path_buf, 0, 128);
     strncpy(path_buf, path_info, strlen(path_info) - strlen(form_feed));
 
-    uwsgi_response_prepare_headers(req, "200 OK", 6);
-    uwsgi_response_add_content_type(req, "application/json", 16);
+    char *path_prefix = "/compute_statistics";
+    if (strncmp(path_prefix, path_buf, strlen(path_prefix)) == 0) {
+        uwsgi_response_prepare_headers_int(req, 200);
+        uwsgi_response_add_content_type(req, "application/json", 16);
 
-    uwsgi_response_write_body_do(req, path_buf, strlen(path_buf));
+
+
+        uwsgi_response_write_body_do(req, path_buf, strlen(path_buf));
+    } else {
+    	uwsgi_response_prepare_headers_int(req, 404);
+    	uwsgi_response_write_body_do(req, "404 Not Found", 13);
+    }
 
     return UWSGI_OK;
 }
