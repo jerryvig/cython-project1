4d9a272060e5ffb6dc7e436ee698aaabc4372f6c
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-03-18 19:31:01 -0500

    updates

--- compute_statistics_main.c
+++ compute_statistics_main.c
@@ -11,43 +11,6 @@
 
 #define BUFFER_SIZE 128
 
-/* int main(void) {
-    const CURL *curl = create_and_init_curl();
-
-    char timestamps[2][12];
-    get_timestamps(timestamps);
-    struct timespec start;
-    struct timespec end;
-
-    int ticker_strlen;
-    char ticker_string[128];
-    while (1) {
-        memset(ticker_string, 0, 128);
-        printf("%s", "Enter ticker list: ");
-        fflush(stdout);
-        if (fgets(ticker_string, 128, stdin) == NULL) {
-            continue;
-        }
-
-        ticker_strlen = strlen(ticker_string) - 1;
-        if (!ticker_strlen) {
-            printf("Got empty ticker string....\n");
-            continue;
-        }
-        ticker_string[ticker_strlen] = 0;
-
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        process_tickers(ticker_string, curl, timestamps);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        printf("processed in %.6f s\n",
-            ((double)end.tv_sec + 1.0e-9*end.tv_nsec) -
-            ((double)start.tv_sec + 1.0e-9*start.tv_nsec));
-    }
-
-    curl_easy_cleanup((CURL*)curl);
-    return EXIT_SUCCESS;
-} */
-
 static uv_loop_t *loop;
 static uv_signal_t sigint_watcher;
 static uv_fs_t stdin_watcher;
